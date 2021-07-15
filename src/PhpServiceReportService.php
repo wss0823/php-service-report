dc90db8c7d29aea073aa9c22ca0338fcb6889bfe
weng <shushun@uniondrug.cn>
2021-07-15 14:44:48 +0800

    初始化

--- src/PhpServiceReportService.php
+++ src/PhpServiceReportService.php
@@ -13,6 +13,18 @@ use Uniondrug\Phar\Server\Services\Http;
 
 class PhpServiceReportService extends Service
 {
+
+    private $postRpt= [
+        'productNo'=>'',//产品编码
+        'moduleNo'=>'',//模块编码
+        'projectVersion'=>'',//模块版本
+        'gitCommitId'=>'',//git commit id
+        'gitTag'=>'',//gitTag
+        'gitBranch'=>'',//git 打包分支
+        'gitCommitTime'=>'',//git 提交时间
+        'serviceIp'=>'',//服务所在ip
+        'serviceNode'=>'',//服务k8s节点名称 环境变量的NODE_NAME
+    ];
     /**
      * @return mixed
      * @throws \Exception
@@ -33,6 +45,38 @@ class PhpServiceReportService extends Service
     //构造函数 初始化时进行服务上报
     public function __construct()
     {
-        echo 1111;
+       //实现上报
+
+    }
+
+    public function getProductNo()
+    {
+        return $this->config->get('app.appName');
+    }
+
+    public function getModuleNo()
+    {
+        return $this->config->get('app.appName');
+    }
+
+    public function getProjectVersion()
+    {
+        return $this->config->get('app.appVersion');
+    }
+
+    public function getGitCommitId()
+    {
+
+        // GIT Commit
+        $buffer = shell_exec("cd '".getcwd()."' && git log -1");
+        if (preg_match("/commit\s+([^\n]+)/i", $buffer, $m) > 0) {
+            $commit = $m[1];
+        }
+       return $commit;
+    }
+
+    public function getGitTag()
+    {
+
     }
 }
